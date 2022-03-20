da4137f2cae22f9347a9c7e332fee7905e7e01bf
minu <gkqkehs10@naver.com>
2022-03-20 16:29:21 +0900

    please god

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -74,7 +74,6 @@ const config: Configuration = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    chunkFilename: '[name].chunk.js',
     publicPath: '/dist/',
   },
   devServer: {
