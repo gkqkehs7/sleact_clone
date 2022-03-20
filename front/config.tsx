3675f20df5f7cafc5f1f8557b760257cf3af67ae
minu <gkqkehs10@naver.com>
2022-03-20 16:34:28 +0900

    please plesase

--- front/config.tsx
+++ front/config.tsx
@@ -1,2 +1,4 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
-export const backUrl = !isDevelopment ? 'http://15.164.103.236/' : 'http://localhost:80';
+// export const backUrl = !isDevelopment ? 'http://localhost:80' : 'http://15.164.103.236/';
+
+export const backUrl = 'http://15.164.103.236/';
