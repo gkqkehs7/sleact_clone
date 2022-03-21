364bf7c925c2b8744499b5ecd36adb34257d2707
minu <gkqkehs10@naver.com>
2022-03-21 14:45:48 +0900

    i know now

--- front/client.tsx
+++ front/client.tsx
@@ -8,8 +8,7 @@ import axios from 'axios';
 import App from './layouts/App';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://15.164.103.236' : 'http://localhost:80';
 
 render(
   <BrowserRouter>
